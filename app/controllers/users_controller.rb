7a10a84f77786bb1a94f9f5c015a6e9b1710aced
PyMountain <lucasandrade664@gmail.com>
2018-09-19 18:00:35 -0300

    capitulo 6/2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,10 @@ class UsersController < ApplicationController
   def new
     @title = "Sign up"
   end
+
+  def show
+    @user = User.find(params[:id])
+    #debugger
+    @title = "User"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
   get 'welcome/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+  resources :users
 end
